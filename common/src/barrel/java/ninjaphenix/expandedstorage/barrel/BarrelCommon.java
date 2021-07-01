652fa8284ae6701806aaa91410fd534c93045da3
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-07-01 12:58:06 +0100

    Make barrel upgrading a bit more robust.

--- common/src/barrel/java/ninjaphenix/expandedstorage/barrel/BarrelCommon.java
+++ common/src/barrel/java/ninjaphenix/expandedstorage/barrel/BarrelCommon.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.LockCode;
 import net.minecraft.world.item.BlockItem;
@@ -47,23 +48,36 @@ public final class BarrelCommon {
         BlockPos pos = context.getClickedPos();
         BlockState state = level.getBlockState(pos);
         Block block = state.getBlock();
-        boolean condition = block instanceof BarrelBlock;
-        if ((condition && ((BarrelBlock) block).blockTier() == from) || !condition && from == Utils.WOOD_TIER.key()) {
-            var toBlock = (AbstractOpenableStorageBlock) BaseApi.getInstance().getTieredBlock(BarrelCommon.BLOCK_TYPE, to);
-            var inventory = NonNullList.withSize(toBlock.getSlotCount(), ItemStack.EMPTY);
-            var tag = level.getBlockEntity(pos).save(new CompoundTag());
-            var code = LockCode.fromTag(tag);
-            ContainerHelper.loadAllItems(tag, inventory);
-            level.removeBlockEntity(pos);
-            var newState = toBlock.defaultBlockState().setValue(BlockStateProperties.FACING, state.getValue(BlockStateProperties.FACING));
-            if (level.setBlockAndUpdate(pos, newState)) {
-                var newEntity = (AbstractOpenableStorageBlockEntity) level.getBlockEntity(pos);
-                var newTag = newEntity.save(new CompoundTag());
-                ContainerHelper.saveAllItems(newTag, inventory);
-                code.addToTag(newTag);
-                newEntity.load(newState, newTag);
-                context.getItemInHand().shrink(1);
-                return true;
+        boolean isExpandedStorageBarrel = block instanceof BarrelBlock;
+        var containerSize = !isExpandedStorageBarrel ? Utils.WOOD_STACK_COUNT : ((BarrelBlock) BaseApi.getInstance().getTieredBlock(BarrelCommon.BLOCK_TYPE, ((BarrelBlock) block).blockTier())).getSlotCount();
+        if (isExpandedStorageBarrel && ((BarrelBlock) block).blockTier() == from || !isExpandedStorageBarrel && from == Utils.WOOD_TIER.key()) {
+            var blockEntity = level.getBlockEntity(pos);
+            var tag = blockEntity.save(new CompoundTag());
+            boolean verifiedSize = blockEntity instanceof Container container && container.getContainerSize() == containerSize;
+            if (!verifiedSize) { // Cannot verify container size, we'll let it upgrade if it has or has less than 27 items
+                if (tag.contains("Items", Utils.NBT_LIST_TYPE)) {
+                    var items = tag.getList("Items", Utils.NBT_COMPOUND_TYPE);
+                    if (items.size() <= containerSize) {
+                        verifiedSize = true;
+                    }
+                }
+            }
+            if (verifiedSize) {
+                var toBlock = (AbstractOpenableStorageBlock) BaseApi.getInstance().getTieredBlock(BarrelCommon.BLOCK_TYPE, to);
+                var inventory = NonNullList.withSize(toBlock.getSlotCount(), ItemStack.EMPTY);
+                var code = LockCode.fromTag(tag);
+                ContainerHelper.loadAllItems(tag, inventory);
+                level.removeBlockEntity(pos);
+                var newState = toBlock.defaultBlockState().setValue(BlockStateProperties.FACING, state.getValue(BlockStateProperties.FACING));
+                if (level.setBlockAndUpdate(pos, newState)) {
+                    var newEntity = (AbstractOpenableStorageBlockEntity) level.getBlockEntity(pos);
+                    var newTag = newEntity.save(new CompoundTag());
+                    ContainerHelper.saveAllItems(newTag, inventory);
+                    code.addToTag(newTag);
+                    newEntity.load(newState, newTag);
+                    context.getItemInHand().shrink(1);
+                    return true;
+                }
             }
         }
         return false;
@@ -74,7 +88,7 @@ public final class BarrelCommon {
     }
 
     public static void registerUpgradeBehaviours(net.minecraft.tags.Tag<Block> tag) {
-        Predicate<Block> isUpgradableChestBlock = (block) -> block instanceof BarrelBlock || tag.contains(block);
+        Predicate<Block> isUpgradableChestBlock = (block) -> block instanceof BarrelBlock || block instanceof net.minecraft.world.level.block.BarrelBlock || tag.contains(block);
         BaseApi.getInstance().defineBlockUpgradeBehaviour(isUpgradableChestBlock, BarrelCommon::tryUpgradeBlock);
     }
 }

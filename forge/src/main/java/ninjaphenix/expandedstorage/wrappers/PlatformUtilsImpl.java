9ee2104c4ca7ed986d8cfd5abe14ecccbd7a5a7d
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-10-15 15:30:45 +0100

    Use forge's supplied inventory wrapper for iitemhandlers.

--- forge/src/main/java/ninjaphenix/expandedstorage/wrappers/PlatformUtilsImpl.java
+++ forge/src/main/java/ninjaphenix/expandedstorage/wrappers/PlatformUtilsImpl.java
@@ -23,8 +23,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.wrapper.InvWrapper;
 import ninjaphenix.expandedstorage.ForgeChestProperties;
 import ninjaphenix.expandedstorage.Utils;
 import ninjaphenix.expandedstorage.block.misc.AbstractOpenableStorageBlockEntity;
@@ -48,99 +47,7 @@ final class PlatformUtilsImpl extends PlatformUtils {
 
     @Override
     public Object createGenericItemAccess(AbstractOpenableStorageBlockEntity entity) {
-        return new IItemHandlerModifiable() {
-            @Override
-            public void setStackInSlot(int slot, ItemStack stack) {
-                entity.getItems().set(slot, stack);
-                if (stack.getCount() > this.getSlotLimit(slot)) {
-                    stack.setCount(this.getSlotLimit(slot));
-                }
-                entity.setChanged();
-            }
-
-            @Override
-            public int getSlots() {
-                return entity.getItems().size();
-            }
-
-            @Override
-            public ItemStack getStackInSlot(int slot) {
-                return entity.getItems().get(slot);
-            }
-
-            @Override
-            public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-                if (stack.isEmpty()) {
-                    return ItemStack.EMPTY;
-                }
-                ItemStack stackInSlot = entity.getItems().get(slot);
-                if (stackInSlot.isEmpty()) {
-                    int limit = this.getSlotLimit(slot);
-                    if (stack.getCount() > limit) {
-                        if (!simulate) {
-                            ItemStack newStack = stack.copy();
-                            newStack.setCount(limit);
-                            entity.getItems().set(slot, newStack);
-                            entity.setChanged();
-                        }
-                        return simulate ? stack.copy().split(stack.getCount() - limit) : stack.split(stack.getCount() - limit);
-                    } else {
-                        if (!simulate) {
-                            entity.getItems().set(slot, stack.copy());
-                            entity.setChanged();
-                        }
-                    }
-                } else if (ItemHandlerHelper.canItemStacksStack(stackInSlot, stack)) {
-                    int limit = Math.min(stackInSlot.getMaxStackSize(), this.getSlotLimit(slot));
-                    int diff = limit - stackInSlot.getCount();
-                    if (diff != 0) {
-                        if (stack.getCount() > diff) {
-                            if (!simulate) {
-                                stackInSlot.setCount(limit);
-                                entity.setChanged();
-                            }
-                            return simulate ? stack.copy().split(stack.getCount() - diff) : stack.split(stack.getCount() - diff);
-                        } else {
-                            if (!simulate) {
-                                stackInSlot.setCount(stackInSlot.getCount() + stack.getCount());
-                                entity.setChanged();
-                            }
-                        }
-                    } else {
-                        return stack;
-                    }
-                } else {
-                    return stack;
-                }
-                return ItemStack.EMPTY;
-            }
-
-            @Override
-            public ItemStack extractItem(int slot, int amount, boolean simulate) {
-                ItemStack stackInSlot = entity.getItems().get(slot);
-                if (stackInSlot.isEmpty()) {
-                    return ItemStack.EMPTY;
-                } else if (amount >= stackInSlot.getCount()) {
-                    if (!simulate) {
-                        entity.getItems().set(slot, ItemStack.EMPTY);
-                    }
-                    return stackInSlot;
-                } else {
-                    ItemStack copy = simulate ? stackInSlot.copy() : stackInSlot;
-                    return copy.split(amount);
-                }
-            }
-
-            @Override
-            public int getSlotLimit(int slot) {
-                return 64;
-            }
-
-            @Override
-            public boolean isItemValid(int slot, ItemStack stack) {
-                return true;
-            }
-        };
+        return new InvWrapper(entity.getInventory());
     }
 
     @Override
